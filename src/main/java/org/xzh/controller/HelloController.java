b419fe2cbf39bad4a4cd5cd62ec71782604b5465
xiaziheng <1192939559@qq.com>
2020-07-31 14:33:24 +0800

    study ci/cd

--- src/main/java/org/xzh/controller/HelloController.java
+++ src/main/java/org/xzh/controller/HelloController.java
@@ -1,7 +1,13 @@
 package org.xzh.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @RestController
 public class HelloController {
@@ -10,5 +16,19 @@ public class HelloController {
     public Object hello() {
         System.out.println("hello, console");
         return "hello, test update to CI 2";
+    }
+
+    @PostMapping("/hook")
+    public Object hook(HttpServletRequest request) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+
+        String str = "";
+        String wholeStr = "";
+        while((str = reader.readLine()) != null){//一行一行的读取body体里面的内容；
+            wholeStr += str;
+        }
+//        String userno = (String) t.get("userId");                   //得到想要的参数
+        System.out.println("success" + wholeStr);
+        return "success";
     }
 }
