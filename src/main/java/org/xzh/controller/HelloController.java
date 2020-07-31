fd3b19d3b5a3334ebfefeb61a4f7e9478660bb90
xiaziheng <1192939559@qq.com>
2020-07-31 19:40:39 +0800

    test github webhook  jenkins

--- src/main/java/org/xzh/controller/HelloController.java
+++ src/main/java/org/xzh/controller/HelloController.java
@@ -30,6 +30,6 @@ public class HelloController {
 //        String userno = (String) t.get("userId");                   //得到想要的参数
         System.out.println("success" + wholeStr);
         return wholeStr;
-        //12
+        //123
     }
 }
